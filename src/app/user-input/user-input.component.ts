a00a94e2f14bdc99b0090fb684f8f4fc8cfbcdb7
SOUMYA\ersau <ersaumyarout@gmail.com>
2025-04-04 18:33:15 +0530

    user input component added

---
+++ src/app/user-input/user-input.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+  selector: 'app-user-input',
+  imports: [
+    FormsModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatButtonModule,
+    MatCardModule,
+  ],
+  templateUrl: './user-input.component.html',
+  styleUrl: './user-input.component.css',
+})
+export class UserInputComponent {}
